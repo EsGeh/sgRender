27102b240b70c3e8d97ed6a20bee7e12bc73b779
EsGeh <SamuelGfroerer@googlemail.com>
2013-10-01 18:58:21 +0200

    experimental version.
    everything compiles quite fine, but many features are still missing.

---
+++ SGRender/Block.hs
@@ -0,0 +1,100 @@
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses #-}
+module SGRender.Block where
+
+import SGRender.Render
+
+import Util.Vector2D
+import Util.Card.Card
+import Util.Card.Unary
+import Math.Matrix
+import Data.Maybe
+import Data.Monoid
+
+data Block a = Block {
+	runBlock :: Matrix a
+}
+instance MultiMonoid (Block a) N2 where
+	mmappend indexDim l r = case toInt indexDim of
+		0 -> horiBlockComb l r
+		1 -> vertBlockComb l r
+		_ -> error "mappend defined in two dimensions only!"
+
+{-
+newtype HoriBlock a = HoriBlock { fromHoriBlock :: Block a }
+newtype VertBlock a = VertBlock { fromVertBlock :: Block a }
+horiBlock = HoriBlock . Block
+vertBlock = VertBlock . Block
+instance Monoid (HoriBlock a) where
+	mempty = horiBlock $ fromJust $ mFromListRow []
+	mappend l r = HoriBlock $ horiBlockComb (fromHoriBlock l) (fromHoriBlock r)
+instance Monoid (VertBlock a) where
+	mempty = vertBlock $ fromJust $ mFromListRow []
+	mappend l r = VertBlock $ vertBlockComb (fromVertBlock l) (fromVertBlock r)
+-}
+instance Show (Block Char) where
+	show block = unlines $ [mGetRow iRow (runBlock block) | iRow <- mGetAllIndexRow (runBlock block)]
+{-
+instance (Show a) => Show (Block a) where
+	show block = show $ (runBlock block)
+-}
+
+horiBlockComb :: ReprComb (Block a)
+horiBlockComb l@(Block matrL) r@(Block matrR) = if (vecX $ mGetSize $ matrL) /= (vecX $ mGetSize $ matrR)
+	then error "horiBlock Exception"
+	else Block $ m (vecX $ mGetSize matrL, (vecY $ mGetSize matrL) + (vecY $ mGetSize matrR)) valFromIndex
+	where
+		valFromIndex index@(row,col) = if col < vecY (mGetSize matrL)
+			then mGet index matrL
+			else mGet (index |-| (0, vecY (mGetSize matrL))) matrR
+
+vertBlockComb :: ReprComb (Block a)
+vertBlockComb l@(Block matrL) r@(Block matrR) = if (vecY $ mGetSize $ matrL) /= (vecY $ mGetSize $ matrR)
+	then error "vertBlock Exception"
+	else Block $ m ((vecX $ mGetSize matrL) + (vecX $ mGetSize matrR), vecY $ mGetSize matrL) valFromIndex
+	where
+		valFromIndex index@(row,col) = if row < vecX (mGetSize matrL)
+			then mGet index matrL
+			else mGet (index |-| (vecX (mGetSize matrL),0)) matrR
+
+renderToBlock :: (src -> String) -> String -> RenderFunctionWithSize src (Block Char) (Size Int)
+renderToBlock show fillTile size list = Block $ fromJust $ mFromListRow $ chop (vecX size) $ take area $ show list ++ cycle fillTile
+	where
+		area = vecX size * vecY size
+
+{-
+renderToHoriBlock fillTile size = HoriBlock . renderToBlock fillTile size
+renderToVertBlock fillTile size = VertBlock . renderToBlock fillTile size
+-}
+
+{-
+stretchMethBlock show fillTile size block@(Block matr) = renderToBlock show fillTile size $ concat [ mGetRow iRow matr | iRow <- mGetAllIndexRow matr ]
+-}
+
+testMethod :: (Show src) => RenderFunctionWithSize (src,src) (Block Char) (Size Int)
+testMethod = combineWithSize2' horiBlockComb (divHoriFromDivDist divE) (renderToBlock show ".") (renderToBlock show "*")
+testMethod2 :: (Show src) => RenderFunctionWithSize (src,src) (Block Char) (Size Int)
+testMethod2 = combineWithSize2' vertBlockComb (divHoriFromDivDist divE) (renderToBlock show ".") (renderToBlock show "*")
+
+--testMeth :: (Card n) => n -> RenderFunctionWithSize (src,src) (Block Char) (Size Int)
+testMeth indexDim = combineWithSize2 indexDim (divHoriFromDivDist divE) (renderToBlock id ".") (renderToBlock id "*")
+
+test = testMeth n2 (10,10) ("left","right")
+
+--testMethod2 = combWithDist (divVertFromDivDist divE) (renderToVertBlock ".") (renderToVertBlock "*")
+
+divE count width = [4,4]
+
+
+type DivSize dist = DivDist (Size dist)
+divHoriFromDivDist :: DivDist dist ->  DivSize dist
+divHoriFromDivDist divDist count size = zip
+	(repeat $ vecY size)
+	(divDist count $ vecX size)
+divVertFromDivDist :: DivDist dist ->  DivSize dist
+divVertFromDivDist divDist count size = zip
+	(divDist count $ vecY size)
+	(repeat $ vecX size)
+
+chop width list = case list of
+	[] -> [] 
+	_ -> take width list : chop width (drop width list)
---
+++ SGRender/Render.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies #-}
+module SGRender.Render where
+
+import Util.Vector2D
+import Util.Card.Card
+import Util.Card.Unary
+import Data.Monoid
+
+-- |represents a way to combine representations:
+type ReprComb repr = repr -> repr -> repr
+-- |calculate a representation for source
+type RenderFunction src dst = src -> dst
+
+-- |a thing that can be concatenated in many ways:
+class MultiMonoid a countDim | a -> countDim where
+	--mmempty :: indexDim -> a
+	mmappend :: Card n => n -> a -> a -> a
+
+hori = n0
+vert = n1
+
+
+combine2 :: ReprComb repr -> RenderFunction srcL repr -> RenderFunction srcR repr -> RenderFunction (srcL,srcR) repr
+combine2 conc rndrFuncL rndrFuncR (srcL,srcR) = rndrFuncL srcL `conc` rndrFuncR srcR
+
+
+type RenderFunctionWithSize src dst size = size -> src -> dst
+type DivDist a = Count -> a -> [a]
+
+combineWithSize2 :: (Card countDim,MultiMonoid repr countDim, Card indexDim) => indexDim -> DivDist dist -> RenderFunctionWithSize srcL repr dist -> RenderFunctionWithSize srcR repr dist -> RenderFunctionWithSize (srcL,srcR) repr dist
+combineWithSize2 indexDim divDist rndrFuncL rndrFuncR size (srcL,srcR) = let sizeL : sizeR : _ = divDist 2 size in
+	mmappend indexDim (rndrFuncL sizeL srcL) (rndrFuncR sizeR srcR)
+
+combineWithSize2' :: ReprComb repr -> DivDist dist -> RenderFunctionWithSize srcL repr dist -> RenderFunctionWithSize srcR repr dist -> RenderFunctionWithSize (srcL,srcR) repr dist
+combineWithSize2' mappend divDist rndrFuncL rndrFuncR size (srcL,srcR) = let sizeL : sizeR : _ = divDist 2 size in
+	(rndrFuncL sizeL srcL) `mappend` (rndrFuncR sizeR srcR)
+
+type Size a = (a, a)
+type Count = Int 
+
+{-
+hori :: Monoid repr => DivDist dist -> RenderFunctionWithSize srcL repr size -> RenderFunctionWithSize srcR repr size -> RenderFunctionWithSize (srcL,srcR) repr size
+hori divDist rndrFuncL rndrFuncR size (srcL,srcR) = let sizeL : sizeR : _ = (divHoriFromDivDist divDist) 2 size in
+	(rndrFuncL sizeL srcL) `mappend` (rndrFuncR sizeR srcR)
+-}
